6d582f189275b9349598b90c3e3a5991c7416692
Rafael Alonso <rafael.palonso@gmail.com>
2020-09-04 10:28:13 -0300

    more tests. our project is now flawless

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fullname returns the full name of the user capitalized" do
+    user = User.new(first_name: 'RAFA', last_name: 'aLONso')
+    assert_equal 'Rafa Alonso', user.fullname
+  end
+
+  test "Checks that user is only valid with first_name" do
+    user = User.new
+    assert_equal false, user.valid?
+
+    user.first_name = 'Me'
+    assert_equal true, user.valid?
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -12,4 +12,29 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Awesome Products"
     assert_selector ".card-product", count: Product.count
   end
+
+  test 'create a new product' do
+    # fazer o login como um usuario
+    login_as users(:george)
+
+    # visitar a pagina de new
+    visit "/products/new"
+
+    save_and_open_screenshot
+
+    # preencher o formulario (colocar nome e tagline do product)
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+
+    save_and_open_screenshot
+
+    # enviar o formulario
+    click_on 'Create Product'
+
+    save_and_open_screenshot
+
+    # verificar que o index agora tem o card que criamos
+    assert_equal root_path, page.current_path
+    assert_text "Change your life: Learn to code"
+  end
 end
